56db0e2ff87ec58f3bfb673b5043447b6773bb7f
Yauheniyaliashkevich <yauheniyaliashkevich@gmail.com>
2021-06-17 20:18:21 -0500

    Monkey added

--- src/Animals/Main.java
+++ src/Animals/Main.java
@@ -3,5 +3,14 @@ package Animals;
 public class Main {
 
     // add new animals
-    
+    public static void main(String[] args) {
+
+
+    Monkey monkey = new Monkey();
+    monkey.name = "Vladik";
+    monkey.age = "29";
+    monkey.color = "Brown";
+
+
+    }
 }
